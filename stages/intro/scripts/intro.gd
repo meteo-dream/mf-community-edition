d399d1ea3360a8ebda5d6ed51572a2efb85e9ff6
Usered238 <30762980+Usered238@users.noreply.github.com>
2026-08-21 04:07:02 +0900

    fix corals bg in lost map 2 & 4

--- stages/intro/scripts/intro.gd
+++ stages/intro/scripts/intro.gd
@@ -19,7 +19,7 @@ func _ready() -> void:
 		display_disclaimer()
 		return
 		
-	await get_tree().create_timer(0.6, false, true, true).timeout
+	await get_tree().create_timer(0.6, false, true, false).timeout
 	print("[Startup] Compiling shaders...")
 	cacher = SHADER_OPTIMIZER.instantiate()
 	cacher.complete.connect(display_disclaimer)
